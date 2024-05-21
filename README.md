ec0436f9456488054a964e8e3dba727635d57772
HeviDevi <devon.r.ross@gmail.com>
2024-05-21 16:23:55 -0400

    updated README

---
+++ README.md
@@ -0,0 +1,26 @@
+# <LogomaticSVG>
+
+## Description
+
+LogomaticSVG is a CLI app that utilizes the inquirer NPM package to create unique SVG files.
+
+## Installation
+
+Go to https://github.com/HeviDevi/LogomaticSVG and copy the repository. Then install Node by visiting https://nodejs.org/en/download/
+After That install Inquirer by typing the following into the command line: 
+npm install inquirer
+Finally, If you would like to run the available tests type the following into the comand line: 
+npm install --save-dev
+
+## Usage
+
+To create a new logo simply type either of the following in the command line & then answer the prompts:
+node index.js
+npm start
+
+Note that you may enter hexadecimal values when prompted for a color
+
+## Tests
+
+If you would like to run a Jest test on shapes.test.js simply enter the following inthe command line:
+npm test
--- index.js
+++ index.js
@@ -1,61 +1,68 @@
+const { Shapes, Circle, Square, Triangle } = require("./lib/shapes");
+const fileSystem = require("fs");
 
-const { Shapes, Circle, Square, Triangle } = require('./lib/shapes');
-const fileSystem = require('fs');
+import("inquirer").then((inquirer) => {
+  inquirer = inquirer.default;
+  const questions = [
+    {
+      type: "list",
+      name: "shape",
+      message: "What shape should the new logo be?",
+      choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+      type: "input",
+      name: "color",
+      message: "What color should the shape be?",
+    },
+    {
+      type: "input",
+      name: "text",
+      message: "What text should appear on the logo?",
+      validate: function (response) {
+        if (response.length === 3) {
+          return true;
+        } else if (response.length !== 3) {
+          return "Only 3 letter answers will be accepted.";
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "What color should the text of the logo be?",
+    },
+  ];
+  
+  inquirer.prompt(questions).then((response) => {
+    let chosenShape;
+    if (response.shape === "Circle") {
+      chosenShape = new Circle(
+        response.color,
+        response.text,
+        response.textColor
+      );
+    } else if (response.shape === "Square") {
+      chosenShape = new Square(
+        response.color,
+        response.text,
+        response.textColor
+      );
+    } else if (response.shape === "Triangle") {
+      chosenShape = new Triangle(
+        response.color,
+        response.text,
+        response.textColor
+      );
+    }
 
-
-
-import('inquirer')
-    .then(inquirer => {
-        inquirer = inquirer.default;
-        const questions = [
-            {
-                type : 'list',
-                name : 'shape',
-                message : 'What shape should the new logo be?',
-                choices : ['Circle', 'Square', 'Triangle']
-            },
-            {
-                type : 'input',
-                name : 'color',
-                message : 'What color should the shape be?',
-            },
-            {
-                type : 'input',
-                name : 'text',
-                message : 'What text should appear on the logo?',
-                validate : function(response){
-                    if (response.length === 3){
-                        return true;
-                    } else if (response.length !== 3){
-                        return 'Only 3 letter answers will be accepted.';
-                    }}
-            },
-            {
-                type: 'input',
-                name: 'textColor',
-                message: 'What color should the text of the logo be?'
-            }
-        ];
-inquirer.prompt(questions)
-    .then((response) => {
-        let chosenShape
-            if (response.shape === 'Circle') {
-                 chosenShape = new Circle(response.color, response.text, response.textColor);
-            } else if (response.shape === 'Square') { 
-                 chosenShape = new Square(response.color, response.text, response.textColor); 
-            } else if (response.shape === 'Triangle') {
-                 chosenShape = new Triangle(response.color, response.text, response.textColor);
-            } 
-    
-            const newLogo = `
+    const newLogo = `
 ${chosenShape.renderShape()}
-            `
-            fileSystem.writeFile('logo.svg', newLogo, (err) => {
-                if (err) throw err;
-                console.log('Generated logo.svg')
-                console.log('LOGO-MATIC!');
-            });
-        })
+            `;
+    fileSystem.writeFile("logo.svg", newLogo, (err) => {
+      if (err) throw err;
+      console.log("Generated logo.svg");
+      console.log("LOGO-MATIC!");
     });
-
-
+  });
+});
